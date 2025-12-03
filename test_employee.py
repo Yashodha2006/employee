11d7b9d040f0db0263cc7d1edbd115ae50be6380
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2025-12-03 12:42:34 +0530

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -1,10 +1,15 @@
-from employee import employee_details
-
-def test_employee_details():
-    expected_output = {
-        "Employee Name": "Alice",
-        "Employee ID": "E1001",
-        "Department": "IT",
-        "Salary": 55000
+def employee_details(name, emp_id, department, salary):
+    return {
+        "Employee Name": name,
+        "Employee ID": emp_id,
+        "Department": department,
+        "Salary": salary
     }
-    assert employee_details("Alice", "E1001", "IT", 55000) == expected_output
+
+if __name__ == "__main__":
+    name = "Alice"
+    emp_id = "E1001"
+    department = "IT"
+    salary = 55000
+
+    print(employee_details(name, emp_id, department, salary))
